4b2d9798b67b23860f34b88a91af219d38f321e8
mdsamiulislam54 <samiulm5332@gmail.com>
2023-11-02 19:58:45 +0600

    fornted mentor - news hompage design complite

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var btn =document.getElementById("mobileMenu");
+    if (btn.style.display === "block") {
+        btn.style.display = "none";
+      } else {
+        btn.style.display = "block";
+      }
+}
